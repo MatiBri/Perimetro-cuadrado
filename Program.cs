4de76b59c3f3fec21c77037e76cff2d8de277b89
Matias Brizuela <63812706+MatiBri@users.noreply.github.com>
2023-04-02 17:22:22 -0300

    Subida de proyecto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetroCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado, productoCuadrado;
+            string perimetro;
+            Console.Write("Ingrese el valor del lado");
+            perimetro = Console.ReadLine();
+            lado = int.Parse(perimetro);
+            productoCuadrado = lado * 4;
+            Console.Write("El producto del cuadrado es:");
+            Console.WriteLine(productoCuadrado);
+            Console.ReadKey();
+
+        }
+    }
+}
